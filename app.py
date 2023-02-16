2c3a60cd863b703295f30585bc8e083d635c39b0
jurgjn <jurgjn@users.noreply.github.com>
2023-02-16 13:13:25 +0100

    Test seaborn heatmaps on community cloud

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import ast, random, urllib.request
-import pandas as pd, streamlit as st, st_aggrid, py3Dmol, stmol
+import matplotlib.pyplot as plt, seaborn as sns, pandas as pd, streamlit as st, st_aggrid, py3Dmol, stmol
 st.set_page_config(layout="wide")
 
 @st.cache_resource #https://docs.streamlit.io/library/advanced-features/caching
@@ -22,21 +22,29 @@ gb.configure_selection('single')
 gb.configure_grid_options(domLayout='normal')
 gridOptions = gb.build()
 
-grid_response = st_aggrid.AgGrid(df_pockets_, gridOptions=gridOptions, height=200, width='100%')
+grid_response = st_aggrid.AgGrid(df_pockets_,
+    gridOptions=gridOptions,
+    height=200,
+    width='100%',
+    enable_enterprise_modules=False,
+)
 if len(grid_response['selected_rows']) > 0:
     af2_id_ = grid_response['selected_rows'][0]['struct_id']
+    resid_ = grid_response['selected_rows'][0]['resid']
 else:
-    af2_id_ = 'W4M1A1'
+    af2_id_ = df_pockets_.head(1).struct_id.squeeze()
+    resid_ = df_pockets_.head(1).resid.squeeze()
+
+pocket_resid_ = ast.literal_eval(resid_)
 
 #selected_indices = st.selectbox('Select structure:', df_pockets_['struct_id'])
-
 # https://github.com/deepmind/alphafold/issues/92#issuecomment-1005495687
 # https://github.com/Intron7/alpha_viewer
 # https://alphafold.ebi.ac.uk/files/AF-A0A1V6PM83-F1-model_v3.pdb
 
 st.write(f'## {af2_id_}')
 pdb_ = read_af2_v3_(af2_id_)
-colors_pocket = {i: '#0072b2' for i in ast.literal_eval(grid_response['selected_rows'][0]['resid'])}
+colors_pocket = {i: '#0072b2' for i in pocket_resid_}
 
 xyzview = py3Dmol.view(data=pdb_, style={'stick':{}})
 xyzview.setStyle({'cartoon': {
@@ -47,3 +55,7 @@ xyzview.setStyle({'cartoon': {
 }}})
 xyzview.setBackgroundColor('#D3D3D3')
 stmol.showmol(xyzview, height = 800, width=800)
+
+fig, ax = plt.subplots()
+sns.heatmap([[1,2,3], [2,3,2]], ax=ax)
+st.write(fig)
