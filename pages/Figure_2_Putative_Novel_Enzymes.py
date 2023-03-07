752bcadbfe656db8c3cc64faab735e836944ad4a
jurgjn <jurgjn@users.noreply.github.com>
2023-03-07 18:54:51 +0100

    Debug row selection cont.-d

--- pages/Figure_2_Putative_Novel_Enzymes.py
+++ pages/Figure_2_Putative_Novel_Enzymes.py
@@ -69,6 +69,7 @@ with tab1:
         width='100%',
         enable_enterprise_modules=False,
     )
+    if not(len(grid_response['selected_rows']) > 0): time.sleep(5)
     if len(grid_response['selected_rows']) > 0:
         af2_id_ = grid_response['selected_rows'][0]['UniProtKB_ac']
         pocket_id_ = grid_response['selected_rows'][0]['pocket_id']
