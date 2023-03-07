d6b0ebe718d6e19fe787cf06c8d1d5affb3fce79
jurgjn <jurgjn@users.noreply.github.com>
2023-03-07 20:48:37 +0100

    Debug row selection cont.-d

--- Introduction.py
+++ Introduction.py
@@ -9,14 +9,14 @@ with open('README.md') as fh:
     st.markdown(fh.read())
 
 st.markdown("""
-Browse an analysis from the sidebar on the left or go directly to an example from the figures:
-- [A0A849TG76](/Figure_2_Putative_Novel_Enzymes?UniProtKB_ac=A0A849TG76)
+Choose an analysis to browse from the sidebar on the left or go directly to an example from the figures in the manuscript:
+- A0A849TG76
 and
-[A0A2D8BRH7](/Figure_2_Putative_Novel_Enzymes?UniProtKB_ac=A0A2D8BRH7)
+A0A2D8BRH7
 from Figure 2B
-- [A0A849ZK06](/Figure_2_Putative_Novel_Enzymes?UniProtKB_ac=A0A849ZK06)
+- A0A849ZK06
 from Figure 2C
-- [S0EUL8](/Figure_2_Putative_Novel_Enzymes?UniProtKB_ac=S0EUL8)
+- S0EUL8
 from Figure 2D
 - [A0A2R8Y619](/Figure_3_Human-Centric_Clusters?UniProtKB_ac=A0A2R8Y619)
 and
